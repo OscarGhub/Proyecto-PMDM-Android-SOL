2704d23f54d83ee06315fe6e9d8cf15c0c8725f0
Laura Blanco <lblancogutierrez@safareyes.es>
2025-11-22 19:22:14 +0100

    calendario is done

---
+++ FruiTask/app/src/main/java/com/example/fruitask/ui/components/ActividadCalendario.kt
@@ -0,0 +1,11 @@
+package com.example.fruitask.ui.components
+
+
+
+data class ActividadCalendario(
+    val year: Int,
+    val month: Int,
+    val day: Int,
+    val tipo: TipoActividad,
+    val titulo: String
+)
---
+++ FruiTask/app/src/main/java/com/example/fruitask/ui/components/TipoActividad.kt
@@ -0,0 +1,5 @@
+package com.example.fruitask.ui.components
+
+enum class TipoActividad {
+    EXAMEN, PROYECTO, TAREA
+}
